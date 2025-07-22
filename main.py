1608d69ab0b8ec8195302538b0d169acd35562ae
EmilyG101 <101006556+EmilyG101@users.noreply.github.com>
2025-07-22 16:47:16 +0000

    add multiplayer mode

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ pygame.display.set_caption("Space Shooter Vertical")
 ASSET_DIR = os.path.join(os.path.dirname(__file__), "assets")
 SHIP_IMG = pygame.image.load(os.path.join(ASSET_DIR, "ship.png"))
 SHIP_IMG = pygame.transform.scale(SHIP_IMG, (50, 40))
+SHIP2_IMG = pygame.image.load(os.path.join(ASSET_DIR, "ship2.png"))
+SHIP2_IMG = pygame.transform.scale(SHIP2_IMG, (50, 40))
 METEOR_IMG = pygame.image.load(os.path.join(ASSET_DIR, "meteor.png"))
 METEOR_IMG = pygame.transform.scale(METEOR_IMG, (40, 40))
 ENEMY_IMG = pygame.image.load(os.path.join(ASSET_DIR, "enemy.png"))
@@ -38,8 +40,8 @@ FONT = pygame.font.SysFont("Arial", 24)
 BIG_FONT = pygame.font.SysFont("Arial", 48)
 clock = pygame.time.Clock()
 
-def draw_player(x, y):
-    WIN.blit(SHIP_IMG, (x, y))
+def draw_player(x, y, img):
+    WIN.blit(img, (x, y))
 
 def draw_bullet(x, y):
     pygame.draw.rect(WIN, YELLOW, (x, y, BULLET_WIDTH, BULLET_HEIGHT))
@@ -63,12 +65,19 @@ def draw_button(text, x, y, w, h, mouse_pos, mouse_click):
         return True
     return False
 
-def game_loop():
+def game_loop(multiplayer=False):
     score = 0
-    player_x = WIDTH // 2 - PLAYER_WIDTH // 2
-    player_y = HEIGHT - PLAYER_HEIGHT - 20
-    player_rect = pygame.Rect(player_x, player_y, PLAYER_WIDTH, PLAYER_HEIGHT)
-    bullets = []
+    # Player 1
+    player1_x = WIDTH // 3 - PLAYER_WIDTH // 2 if multiplayer else WIDTH // 2 - PLAYER_WIDTH // 2
+    player1_y = HEIGHT - PLAYER_HEIGHT - 20
+    player1_rect = pygame.Rect(player1_x, player1_y, PLAYER_WIDTH, PLAYER_HEIGHT)
+    player1_bullets = []
+    # Player 2 (if multiplayer)
+    if multiplayer:
+        player2_x = WIDTH * 2 // 3 - PLAYER_WIDTH // 2
+        player2_y = HEIGHT - PLAYER_HEIGHT - 20
+        player2_rect = pygame.Rect(player2_x, player2_y, PLAYER_WIDTH, PLAYER_HEIGHT)
+        player2_bullets = []
     meteors = []
     enemies = []
     meteor_timer = 0
@@ -85,15 +94,26 @@ def game_loop():
                 return "quit"
 
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_LEFT] and player_x > 0:
-            player_x -= PLAYER_SPEED
-        if keys[pygame.K_RIGHT] and player_x < WIDTH - PLAYER_WIDTH:
-            player_x += PLAYER_SPEED
+        # Player 1 controls
+        if keys[pygame.K_LEFT] and player1_x > 0:
+            player1_x -= PLAYER_SPEED
+        if keys[pygame.K_RIGHT] and player1_x < WIDTH - PLAYER_WIDTH:
+            player1_x += PLAYER_SPEED
         if keys[pygame.K_SPACE]:
-            if len(bullets) == 0 or bullets[-1][1] < player_y - 20:
-                bullets.append([player_x + PLAYER_WIDTH // 2 - BULLET_WIDTH // 2, player_y])
+            if len(player1_bullets) == 0 or player1_bullets[-1][1] < player1_y - 20:
+                player1_bullets.append([player1_x + PLAYER_WIDTH // 2 - BULLET_WIDTH // 2, player1_y])
+        player1_rect.x = player1_x
 
-        player_rect.x = player_x
+        # Player 2 controls (if multiplayer)
+        if multiplayer:
+            if keys[pygame.K_a] and player2_x > 0:
+                player2_x -= PLAYER_SPEED
+            if keys[pygame.K_d] and player2_x < WIDTH - PLAYER_WIDTH:
+                player2_x += PLAYER_SPEED
+            if keys[pygame.K_LSHIFT]:
+                if len(player2_bullets) == 0 or player2_bullets[-1][1] < player2_y - 20:
+                    player2_bullets.append([player2_x + PLAYER_WIDTH // 2 - BULLET_WIDTH // 2, player2_y])
+            player2_rect.x = player2_x
 
         # Meteors from top, angled down
         meteor_timer += 1
@@ -109,7 +129,7 @@ def game_loop():
             m[1] += m[3] * math.sin(m[2])
             meteor_rect = pygame.Rect(m[0] - METEOR_WIDTH//2, m[1] - METEOR_HEIGHT//2, METEOR_WIDTH, METEOR_HEIGHT)
             draw_meteor(m[0], m[1], m[2])
-            if meteor_rect.colliderect(player_rect):
+            if meteor_rect.colliderect(player1_rect) or (multiplayer and meteor_rect.colliderect(player2_rect)):
                 game_over = True
             if m[1] > HEIGHT:
                 meteors.remove(m)
@@ -126,28 +146,49 @@ def game_loop():
             e[1] += ENEMY_SPEED
             enemy_rect = pygame.Rect(e[0], e[1], ENEMY_WIDTH, ENEMY_HEIGHT)
             draw_enemy(e[0], e[1])
-            if enemy_rect.colliderect(player_rect):
+            if enemy_rect.colliderect(player1_rect) or (multiplayer and enemy_rect.colliderect(player2_rect)):
                 game_over = True
             if e[1] > HEIGHT:
                 enemies.remove(e)
 
-        # Bullets shoot up
-        for b in bullets[:]:
+        # Player 1 bullets
+        for b in player1_bullets[:]:
             b[1] -= BULLET_SPEED
             draw_bullet(b[0], b[1])
             if b[1] < -BULLET_HEIGHT:
-                bullets.remove(b)
+                player1_bullets.remove(b)
             else:
                 for e in enemies[:]:
                     enemy_rect = pygame.Rect(e[0], e[1], ENEMY_WIDTH, ENEMY_HEIGHT)
                     bullet_rect = pygame.Rect(b[0], b[1], BULLET_WIDTH, BULLET_HEIGHT)
                     if bullet_rect.colliderect(enemy_rect):
                         enemies.remove(e)
-                        if b in bullets:
-                            bullets.remove(b)
+                        if b in player1_bullets:
+                            player1_bullets.remove(b)
                         score += 5
 
-        draw_player(player_x, player_y)
+        # Player 2 bullets (if multiplayer)
+        if multiplayer:
+            for b in player2_bullets[:]:
+                b[1] -= BULLET_SPEED
+                draw_bullet(b[0], b[1])
+                if b[1] < -BULLET_HEIGHT:
+                    player2_bullets.remove(b)
+                else:
+                    for e in enemies[:]:
+                        enemy_rect = pygame.Rect(e[0], e[1], ENEMY_WIDTH, ENEMY_HEIGHT)
+                        bullet_rect = pygame.Rect(b[0], b[1], BULLET_WIDTH, BULLET_HEIGHT)
+                        if bullet_rect.colliderect(enemy_rect):
+                            enemies.remove(e)
+                            if b in player2_bullets:
+                                player2_bullets.remove(b)
+                            score += 5
+
+        # Draw players
+        draw_player(player1_x, player1_y, SHIP_IMG)
+        if multiplayer:
+            draw_player(player2_x, player2_y, SHIP2_IMG)
+
         score_text = FONT.render(f"Score: {score}", True, WHITE)
         WIN.blit(score_text, (10, 10))
 
@@ -166,6 +207,24 @@ def game_loop():
             continue
 
         pygame.display.flip()
+
+def mode_menu():
+    while True:
+        WIN.blit(BG_IMG, (0, 0))
+        title = BIG_FONT.render("Select Mode", True, WHITE)
+        WIN.blit(title, (WIDTH//2 - title.get_width()//2, HEIGHT//2 - 100))
+        mouse_pos = pygame.mouse.get_pos()
+        mouse_click = pygame.mouse.get_pressed()[0]
+        if draw_button("Single Player", WIDTH//2 - 150, HEIGHT//2, 140, 50, mouse_pos, mouse_click):
+            return False
+        if draw_button("Multiplayer", WIDTH//2 + 10, HEIGHT//2, 140, 50, mouse_pos, mouse_click):
+            return True
+        pygame.display.flip()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+        pygame.time.wait(100)
 
 def main_menu():
     while True:
@@ -186,7 +245,8 @@ def main_menu():
 def main():
     while True:
         main_menu()
-        result = game_loop()
+        multiplayer = mode_menu()
+        result = game_loop(multiplayer)
         if result == "quit":
             break
 
